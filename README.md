c0587783831377259199de1ffeb757c5e8c1a9f2
david.ils <david.ils@logiball.de>
2022-07-25 12:23:39 +0200

    - cleanup - adjusted README - adjusted folder structure

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ This project is part of the [The Complete JavaScript Course 2022: From Zero to E
 
 In this project, the HTML and CSS files were already given. This is the finale project of the first chapter and the goal is to use the previously learned knowledge to finish this game. DOM manipulation, EventListeners, usage of class lists and storing the active player were all part of this project.
 
+The functions have been implemented based on the following flow chart:
+![Pig Game Flowchart](pig-game-flowchart.png)
+
 ### About the game
 
 This is a two player game. The goal is to reach a minimum score of 100 by rolling the dice. The active player can roll as many times as desired. To save the current score make sure to use the HOLD button. If a 1 is rolled the current score is lost and the next players turn starts.
--- script/script.js
+++ script/script.js
@@ -1,22 +1,32 @@
 'use strict';
 
-// init values
-let activePlayer = 0;
-const scores = [0, 0];
-let currentScore = 0;
-let playing = true;
-
 // select ui elements
 const dice = document.querySelector('.dice');
 const newRoundButton = document.querySelector('.btn--new');
 const rollDiceButton = document.querySelector('.btn--roll');
 const holdButton = document.querySelector('.btn--hold');
 
-// assign start values
-document.getElementById(`score--0`).textContent = 0;
-document.getElementById(`score--1`).textContent = 0;
-document.getElementById(`current--0`).textContent = 0;
-document.getElementById(`current--1`).textContent = 0;
+let activePlayer, scores, currentScore, playing;
+
+const init = function () {
+  activePlayer = 0;
+  scores = [0, 0];
+  currentScore = 0;
+  playing = true;
+
+  document.getElementById(`score--0`).textContent = 0;
+  document.getElementById(`score--1`).textContent = 0;
+  document.getElementById(`current--0`).textContent = 0;
+  document.getElementById(`current--1`).textContent = 0;
+
+  document.querySelector(`.player--0`).classList.remove('player--winner');
+  document.querySelector(`.player--1`).classList.remove('player--winner');
+  document.querySelector(`.player--0`).classList.add('player--active');
+  document.querySelector(`.player--1`).classList.remove('player--active');
+  dice.classList.add('hidden');
+};
+
+init();
 
 const swapPlayer = function () {
   currentScore = 0;
@@ -30,12 +40,16 @@ const swapPlayer = function () {
     .classList.add('player--active');
 };
 
+const rollDice = function () {
+  return Math.trunc(Math.random() * 6) + 1;
+};
+
 // roll dice
 rollDiceButton.addEventListener('click', function () {
   if (playing) {
-    const diceRoll = Math.trunc(Math.random() * 6) + 1;
+    const diceRoll = rollDice();
     dice.classList.remove('hidden');
-    dice.src = `dice-${diceRoll}.png`;
+    dice.src = `img/dice-${diceRoll}.png`;
     if (diceRoll === 1) {
       swapPlayer();
     } else {
@@ -65,23 +79,10 @@ holdButton.addEventListener('click', function () {
   }
 });
 
-// reset
+// reset game
 newRoundButton.addEventListener('click', function () {
-  playing = true;
   document.querySelector(`#name--${activePlayer}`).textContent = `Player ${
     activePlayer + 1
   }`;
-  document
-    .querySelector(`.player--${activePlayer}`)
-    .classList.remove('player--winner');
-  dice.classList.remove('hidden');
-  activePlayer = 1;
-  currentScore = 0;
-  scores[0] = 0;
-  scores[1] = 0;
-  document.getElementById(`score--0`).textContent = 0;
-  document.getElementById(`score--1`).textContent = 0;
-  document.getElementById(`current--0`).textContent = 0;
-  document.getElementById(`current--1`).textContent = 0;
-  swapPlayer();
+  init();
 });
